0805a27009cccdfde6f1a40dceeb38f6562ea38e
Keerthi <2430304@cognizant.com>
2025-12-09 09:54:52 +0530

    List comphersion

--- day11/listcomp.py
+++ day11/listcomp.py
@@ -1,3 +1,4 @@
+# printing even numbers from 0 to 100
 even_num=[i for i in range(0,100) if i%2==0]
 print("even_numbers from 0 to 100:\n",even_num,"\n")
 # Printing even number square from 0 to 100
