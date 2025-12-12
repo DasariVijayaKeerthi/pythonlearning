33e9fdf55d87c71f4174238b753fc12a81a2c0f7
Keerthi <2430304@cognizant.com>
2025-12-12 12:55:57 +0530

    inheritance

--- python/classPractice/inheritance.py
+++ python/classPractice/inheritance.py
@@ -6,3 +6,8 @@ class Dog(Animal):
         print("Bark")
 obj=Dog()
 obj.sound()
+"""
+self keyword is used in the method as the first parameter because it refer to the current object.
+ and self will be first parameter when we write the method inside the class.
+ 
+"""
