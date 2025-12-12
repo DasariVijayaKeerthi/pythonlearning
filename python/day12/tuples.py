4c783bd25b3bfb860314d456bef8604d0b282f7f
Keerthi <2430304@cognizant.com>
2025-12-12 13:14:14 +0530

    tuple

---
+++ python/day12/tuples.py
@@ -0,0 +1,9 @@
+t=(1,2,3,1)
+'''t[0]=10
+ t[0]=10
+    ~^^^
+TypeError: 'tuple' object does not support item assignment'''
+#counts number of times element 1 is repeated
+print(t.count(1))
+# print element at index 0
+print(t[0])
